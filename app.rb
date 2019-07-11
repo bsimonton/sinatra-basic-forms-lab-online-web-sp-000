bf1fa44c1f7d91a85444613485adda4ed821f575
Brian Simonton <bsimonton78@gmail.com>
2019-07-11 06:35:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,22 @@ require_relative 'models/puppy.rb'
 
 class App < Sinatra::Base
  
- 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do
+    months_old = params[:age]
+    name = params[:name]
+    breed = params[:breed]
+    @new_puppy = Puppy.new(name, breed, months_old)
+
+    erb :display_puppy
+  end
 
  
  
